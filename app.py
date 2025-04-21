ec1d8cedfb34276a0619828af1e65ad12a608683
GodgonShlem <fediazavr228@gmail.com>
2025-04-21 15:28:11 +0300

    Создан шаблон pattern
    Создан шаблон pattern со стилями для верхней и нижней части сайта, включен на все существующие страницы.

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', active_page='home')
 
 @app.route('/lessons', methods=['GET','POST'])
 def lessons():
-    return render_template('lessons.html')
+    return render_template('lessons.html', active_page='lessons')
 
 @app.route('/account', methods=['GET','POST'])
 def account():
-    return render_template('account.html')
+    return render_template('account.html', active_page='account')
 
 if __name__ == "__main__":
     app.run(debug=True)
