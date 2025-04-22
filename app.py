9558dad354f89e60d013d28fbf9265dd65366844
GodgonShlem <fediazavr228@gmail.com>
2025-04-22 13:05:09 +0300

    Таблица для пользователей
    Создана таблица для пользователей

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'QWERTYqwety123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///siteBases.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class UsersDB(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    phone = db.Column(db.String(15), nullable=False) 
+    status = db.Column(db.String(15), default='user', nullable=False) 
+    password_hash = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.DateTime(), default=datetime.now)
 
 @app.route('/', methods=['GET','POST'])
 def index():
